ef484e0e6da2f5b6d7fcb748a9bcb81a25f87f64
Ihor Saievych <55617503+ihorsaievych@users.noreply.github.com>
2022-09-30 16:54:38 +0300

    finaly

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+//import * as basicLightbox from "basiclightbox";
+
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  basicLightbox:  window.basicLightbox,
+  instance: {},
+};
+
+const markup = galleryItems.reduce(
+  (acc, galleryItem) =>
+    acc +
+    `<div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+        />
+      </a>
+    </div>`, "");
+
+function onItem(event) {
+  event.preventDefault();
+  if (event.currentTarget === event.target) {
+    return;
+  }
+
+  if (event.target.classList.contains("gallery__image")) {
+    const src = event.target.dataset.source;
+    showModal(src);
+  }
+}
+
+function showModal(src){
+    refs.instance = refs.basicLightbox.create(`<img src="${src}" width="800" height="600">`);   
+    refs.instance.show();
+    
+    window.addEventListener("keydown", onKeyDown);
+}
+    
+const onKeyDown = (event) => {
+    if (event.key !== "Escape") return;
+    refs.instance.close();
+    window.removeEventListener("keydown", onKeyDown);
+}   
+
+refs.gallery.insertAdjacentHTML("beforeend", markup);
+
+refs.gallery.addEventListener("click", onItem);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const refs = {
+    gallery : document.querySelector(".gallery"),
+    simpleLightbox : window.SimpleLightbox,
+  };
+
+  
+const markup = galleryItems.reduce((acc, galleryItem) =>
+    acc + `<a class="gallery__item" href="${galleryItem.original}">
+          <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+       </a>`, "");
+       
+refs.gallery.insertAdjacentHTML("beforeend", markup);
+const lightbox = new refs.simpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
+//console.log(lightbox);
+     
